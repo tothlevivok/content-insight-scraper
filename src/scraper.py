b9c6390a7789fca9dd3d93b64dbaa4850898b4f5
levit <levit@DESKTOP-8PM2QKM>
2026-01-22 16:37:34 +0100

    BeutifulSoup package installed/imported, and job cards selected.

--- src/scraper.py
+++ src/scraper.py
@@ -1,5 +1,17 @@
 import requests
+from bs4 import BeautifulSoup
+
 URL = "https://realpython.github.io/fake-jobs/"
 page = requests.get(URL)
 
-print(page.text)
+#print(page.text)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id="ResultsContainer")
+print(results.prettify())
+
+job_cards = results.find_all("div", class_="card-content")
+
+for job_card in job_cards:
+    print(job_card, end="\n" * 2)
